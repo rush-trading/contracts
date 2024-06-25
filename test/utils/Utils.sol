2b502e2bbf6a1846cc21edab4cd4ed1df755ce73
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@protonmail.com>
2024-06-25 16:35:06 -0400

    test: remove "StdUtils" from "Utils" contract

--- test/utils/Utils.sol
+++ test/utils/Utils.sol
@@ -2,9 +2,8 @@
 pragma solidity >=0.8.25;
 
 import { CommonBase } from "forge-std/src/Base.sol";
-import { StdUtils } from "forge-std/src/StdUtils.sol";
 
-abstract contract Utils is CommonBase, StdUtils {
+abstract contract Utils is CommonBase {
     /// @dev Stops the active prank and sets a new one.
     function resetPrank(address msgSender) internal {
         vm.stopPrank();
