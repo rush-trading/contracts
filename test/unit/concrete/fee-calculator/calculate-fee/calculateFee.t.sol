ba22542758c0f81fdc5369ab64558fd1dc8bc383
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@protonmail.com>
2024-06-24 20:41:42 -0400

    test: remove TODOs from "test_GivenUtilizationIsGreaterThanOptimalUtilization" tests

--- test/unit/concrete/fee-calculator/calculate-fee/calculateFee.t.sol
+++ test/unit/concrete/fee-calculator/calculate-fee/calculateFee.t.sol
@@ -12,7 +12,6 @@ contract CalculateFee_Unit_Concrete_Test is FeeCalculator_Unit_Shared_Test {
     }
 
     function test_GivenUtilizationIsGreaterThanOptimalUtilization() external view {
-        // TODO: use Defaults utility to get the default values
         uint256 duration = 31_536_000; // 1 year
         uint256 newLiquidity = 50_000e18; // 50K tokens
         uint256 outstandingLiquidity = 900_000e18; // 900K tokens
@@ -31,7 +30,6 @@ contract CalculateFee_Unit_Concrete_Test is FeeCalculator_Unit_Shared_Test {
             })
         );
 
-        // TODO: replace with scripts to calculate the expected values
         uint256 expectedTotalFee = 83_312_499_995_352_000_000_000;
         uint256 expectedReserveFee = 8_331_249_999_535_200_000_000;
         assertEq(actualTotalFee, expectedTotalFee);
@@ -57,7 +55,6 @@ contract CalculateFee_Unit_Concrete_Test is FeeCalculator_Unit_Shared_Test {
             })
         );
 
-        // TODO: replace with scripts to calculate the expected values
         uint256 expectedTotalFee = 5_007_499_999_725_600_000_000;
         uint256 expectedReserveFee = 500_749_999_972_560_000_000;
         assertEq(actualTotalFee, expectedTotalFee);
