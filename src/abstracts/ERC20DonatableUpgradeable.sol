0419994c1e478489a0334aef974076382a9be252
Vinny <vinny@rush.trading>
2024-11-11 14:55:50 +0000

    wip

---
+++ src/abstracts/ERC20DonatableUpgradeable.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.26;
+
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {ILiquidityDeployer} from "src/interfaces/ILiquidityDeployer.sol";
+import { LD } from "src/types/DataTypes.sol";
+import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+
+/**
+ * @dev Extension of {ERC20} that includes a donation to a given, well known address.
+ */
+abstract contract ERC20DonatableUpgradeable is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+
+
+    /// @notice raise when token has not yet been around. In order to maintain invariant, donation only available
+    /// after
+    error ERC20Donatable_NotUnwoundYet();
+
+    error ERC20Donatable_DonationAlreadySent();
+
+
+    // #region ------------------------------------=|+ EVENTS +|=------------------------------------ //
+
+    event Donation(address indexed receiver, uint256 amount);
+
+    // #endregion ----------------------------------------------------------------------------------- //
+
+    // #region --------------------------------=|+ PUBLIC STORAGE +|=-------------------------------- //
+
+    /// @notice Reciever of the donation
+    address public donationBeneficiary;
+
+    /// @notice UniV2 Pool address TODO: can change to use create2 to derive
+    address uniPool;
+
+    uint256 public constant FIXED_DONATION_AMOUNT = 10000000e18;
+    /// @notice Liquidity Deployer, the only address allowed to call donate
+    ILiquidityDeployer public liquidityDeployer;
+
+    bool public isDonationSent = false;
+
+
+    // #endregion ----------------------------------------------------------------------------------- //
+
+
+
+    // #region -----------------------=|+ INTERNAL NON-CONSTANT FUNCTIONS +|=------------------------ //
+
+    /// @dev Initialize the contract with calls to parent initializers.
+
+    function __ERC20Donatable_init(
+        address _donationBeneficiary,
+        address _liquidityDeployer,
+        address _uniPool
+    ) internal
+      onlyInitializing {
+
+        __ERC20Donatable_init_unchained(_donationBeneficiary, _liquidityDeployer, _uniPool);
+
+    }
+
+
+
+
+
+    /// @dev Initialize the contract without calling parent initializers.
+    function __ERC20Donatable_init_unchained(
+        address _donationBeneficiary,
+        address _liquidityDeployer,
+        address _uniPool
+    )
+        internal
+        onlyInitializing
+    {
+        donationBeneficiary = _donationBeneficiary;
+        liquidityDeployer = ILiquidityDeployer(_liquidityDeployer);
+        uniPool = _uniPool;
+    }
+
+
+    // #endregion ----------------------------------------------------------------------------------- //
+
+
+    // #region -----------------------=|+ PUBLIC  FUNCTIONS +|=------------------------ //
+    function donate() public {
+        LD.LiquidityDeployment memory liqDeployment = liquidityDeployer.getLiquidityDeployment(uniPool);
+        if (!liqDeployment.isUnwound) {
+            revert ERC20Donatable_NotUnwoundYet();
+        }
+
+        if (isDonationSent) {
+            revert ERC20Donatable_DonationAlreadySent();
+        }
+
+        _mint(donationBeneficiary, FIXED_DONATION_AMOUNT);
+        isDonationSent = true;
+
+        emit Donation(donationBeneficiary, FIXED_DONATION_AMOUNT);
+
+    }
+
+}
---
+++ src/tokens/RushERC20Donatable.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.26;
+
+import { ERC20DonatableUpgradeable } from "src/abstracts/ERC20DonatableUpgradeable.sol";
+import { IRushERC20, RushERC20Abstract } from "src/abstracts/RushERC20Abstract.sol";
+
+/**
+ * @title RushERC20Donatable
+ * @notice The Donatable Rush ERC20 token implementation.
+ */
+contract RushERC20Donatable is ERC20DonatableUpgradeable, RushERC20Abstract {
+    // #region -----------------------------------=|+ STRUCTS +|=------------------------------------ //
+
+    struct InitializeLocalVars {
+        address donationBeneficiary;
+        address liquidityDeployer;
+        address uniPool;
+    }
+
+    // #endregion ----------------------------------------------------------------------------------- //
+
+    // #region ------------------------------=|+ CONSTANT FUNCTIONS +|=------------------------------ //
+
+    /// @inheritdoc IRushERC20
+    function description() external pure override returns (string memory) {
+        return "RushERC20Donatable";
+    }
+
+    /// @inheritdoc IRushERC20
+    function version() external pure override returns (uint256) {
+        return 1;
+    }
+
+    // #endregion ----------------------------------------------------------------------------------- //
+
+    // #region ----------------------------=|+ NON-CONSTANT FUNCTIONS +|=---------------------------- //
+
+    /// @inheritdoc IRushERC20
+    function initialize(
+        string calldata name,
+        string calldata symbol,
+        uint256 maxSupply,
+        address recipient,
+        bytes calldata data
+    )
+        external
+        override
+        initializer
+    {
+        InitializeLocalVars memory vars;
+        __ERC20_init(name, symbol);
+        _mint(recipient, maxSupply);
+        (vars.donationBeneficiary, vars.liquidityDeployer, vars.uniPool) =
+            abi.decode(data, (address, address, address));
+        __ERC20Donatable_init({
+            _donationBeneficiary: vars.donationBeneficiary,
+            _liquidityDeployer: vars.liquidityDeployer,
+            _uniPool: vars.uniPool
+        });
+        emit Initialize({ name: name, symbol: symbol, maxSupply: maxSupply, recipient: recipient, data: data });
+    }
+
+    // #endregion ----------------------------------------------------------------------------------- //
+}
