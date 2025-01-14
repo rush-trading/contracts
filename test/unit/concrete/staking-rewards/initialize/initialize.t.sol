8c0d3a178d22f62b1bf969b6f8eb3f89df30bfb4
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@protonmail.com>
2025-01-14 15:08:09 -0400

    test: add Initialize_Integration_Concrete_Test test: add "initialize" function test tree

---
+++ test/unit/concrete/staking-rewards/initialize/initialize.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.26 <0.9.0;
+
+import { Errors } from "src/libraries/Errors.sol";
+import { StakingRewards_Unit_Concrete_Test } from "test/unit/concrete/staking-rewards/StakingRewards.t.sol";
+
+contract Initialize_Integration_Concrete_Test is StakingRewards_Unit_Concrete_Test {
+    function test_RevertGiven_AlreadyInitialized() external {
+        // Initialize the contract.
+        stakingRewards.initialize({ token_: address(rushERC20Mock) });
+
+        // Run the test.
+        vm.expectRevert(abi.encodeWithSelector(Errors.InvalidInitialization.selector));
+        stakingRewards.initialize({ token_: address(rushERC20Mock) });
+    }
+
+    function test_GivenNotInitialized() external {
+        // Mint some tokens to the contract as rewards.
+        rushERC20Mock.mint({ account: address(stakingRewards), amount: defaults.STAKING_AMOUNT() });
+
+        // Expect the relevant event to be emitted.
+        vm.expectEmit({ emitter: address(stakingRewards) });
+        emit Initialize({ reward: defaults.STAKING_AMOUNT() });
+
+        // Initialize the contract.
+        stakingRewards.initialize({ token_: address(rushERC20Mock) });
+
+        // Assert that the contract was initialized correctly.
+        address actualToken = address(stakingRewards.token());
+        address expectedName = address(rushERC20Mock);
+        assertEq(actualToken, expectedName, "token");
+
+        uint256 actualRewardsDuration = stakingRewards.rewardsDuration();
+        uint256 expectedRewardsDuration = 180 days;
+        assertEq(actualRewardsDuration, expectedRewardsDuration, "rewardsDuration");
+
+        uint256 actualRewardRate = stakingRewards.rewardRate();
+        uint256 expectedRewardRate = defaults.STAKING_AMOUNT() / stakingRewards.rewardsDuration();
+        assertEq(actualRewardRate, expectedRewardRate, "rewardRate");
+
+        uint256 actualLastUpdateTime = stakingRewards.lastUpdateTime();
+        uint256 expectedLastUpdateTime = block.timestamp;
+        assertEq(actualLastUpdateTime, expectedLastUpdateTime, "lastUpdateTime");
+
+        uint256 actualPeriodFinish = stakingRewards.periodFinish();
+        uint256 expectedPeriodFinish = block.timestamp + stakingRewards.rewardsDuration();
+        assertEq(actualPeriodFinish, expectedPeriodFinish, "periodFinish");
+    }
+}
